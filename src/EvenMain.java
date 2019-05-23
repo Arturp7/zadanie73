6623536e601e9669f394aedcaf796978f13f7b96
Artur Przyjemski <spinezpl@gmail.com>
2019-05-23 19:38:53 +0200

    first commit

---
+++ src/EvenMain.java
@@ -0,0 +1,20 @@
+public class EvenMain {
+    public static void main(String[] args) {
+        int i = 0;
+        int sum = 0;
+
+        while (i<=100){
+            if(i%2==0)
+            System.out.println(i);
+            sum = sum + i;
+            i++;
+
+           }
+        System.out.println(sum);
+
+
+
+
+    }
+
+}
